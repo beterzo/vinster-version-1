11bd5ec4af08df1d005d8f7366ba91defbc7a961
beterzo <hi@beterzo.tech>
2025-07-07 09:50:31 +0200

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -94,7 +94,7 @@ const LoginPage = () => {
             <img 
               alt="Vinster Logo" 
               onClick={() => navigate('/')} 
-              src="/lovable-uploads/vinster-new-logo.png" 
+              src="/lovable-uploads/0a60c164-79b3-4ce8-80cb-a3d37886f987.png" 
               className="h-20 w-auto cursor-pointer hover:opacity-80 transition-opacity duration-200" 
             />
             <LanguageSwitcher />
