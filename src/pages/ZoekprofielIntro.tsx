5493bf0dc47cef10626717dcd6f22e6044282fe2
beterzo <hi@beterzo.tech>
2025-07-07 09:40:04 +0200

    Update ZoekprofielIntro.tsx

--- src/pages/ZoekprofielIntro.tsx
+++ src/pages/ZoekprofielIntro.tsx
@@ -16,7 +16,7 @@ const ZoekprofielIntro = () => {
               alt="Vinster Logo" 
               className="h-12 w-auto cursor-pointer hover:opacity-80 transition-opacity duration-200" 
               onClick={() => navigate('/home')} 
-              src="/lovable-uploads/208c47cf-042c-4499-94c1-33708e0f5639.png" 
+              src="/lovable-uploads/0a60c164-79b3-4ce8-80cb-a3d37886f987.png" 
             />
           </div>
         </div>
